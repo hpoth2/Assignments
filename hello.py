53ed2970b2aabcf3b94be68544b3fefcf5f701b4
hpoth2 <potham.haripriya@gmail.com>
2016-02-09 01:01:03 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+import sys
+
+# Define a main() function that prints a little greeting.
+def main():
+  # Get the name from the command line, using 'World' as a fallback.
+
+  print 'Hello World'
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
